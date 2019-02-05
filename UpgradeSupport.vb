188e905115efdb4d34736fa0c3aab9df08481942
Colleen <colleen.wade@branz.co.nz>
2019-02-05 14:16:10 +1300

    misc

--- UpgradeSupport.vb
+++ UpgradeSupport.vb
@@ -1,4 +1,4 @@
 Module UpgradeSupport
     'Friend DAODBEngine_definst As New dao.DBEngine
-	Friend ExcelGlobal_definst As New Microsoft.Office.Interop.Excel.Global
+    'Friend ExcelGlobal_definst As New Microsoft.Office.Interop.Excel.Global
 End Module
